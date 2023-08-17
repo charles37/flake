de24aec225737ce3701f71cf3d917a17857bd396
charle37 <benprevor@gmail.com>
2023-08-17 14:43:03 -0400

    testing server setup on start

--- modules/programs/neovim/after/plugin/lsp.lua
+++ modules/programs/neovim/after/plugin/lsp.lua
@@ -1,5 +1,5 @@
 local lsp = require('lsp-zero').preset({
-  setup_servers_on_start = false,
+  setup_servers_on_start = true,
 })
 
 lsp.preset("recommended")
@@ -50,7 +50,6 @@ lsp.set_preferences({
 
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
-
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
