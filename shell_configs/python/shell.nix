855dc5d68032a6fdd0a17475ed97f06d2fc6a008
charle37 <benprevor@gmail.com>
2023-09-22 11:25:34 -0400

    added some developer shell configs for certain languages

---
+++ shell_configs/python/shell.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  my-python-packages = ps: with ps; [
+    pandas
+    requests
+    # other python packages
+  ];
+  my-python = pkgs.python3.withPackages my-python-packages;
+in my-python.env
