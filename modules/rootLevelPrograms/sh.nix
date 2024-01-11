7a941fe4ed91f8c94f5ed4bcfd0fd1d1ffabe02e
charle37 <benprevor@gmail.com>
2024-01-11 11:59:46 -0500

    zsh

--- modules/rootLevelPrograms/sh.nix
+++ modules/rootLevelPrograms/sh.nix
@@ -13,12 +13,18 @@ in
   };
 
   programs.zsh = {
+    histSize = 10000;
+    histFile = "${config.xdg.dataHome}/zsh/history";
     enable = true;
     enableCompletion = true;
-    plugins = [
-      { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
-      { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
-    ];
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
+      ];
+    };
+
     shellAliases = myAliases;
   };
 }
